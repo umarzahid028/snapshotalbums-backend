9a88729fa7335e48dc6b40f2b8b894660725585c
Umar Zahid <umarzahid028@gmail.com>
2025-10-29 18:27:17 +0500

    ..

--- app/Http/Controllers/Api/V1/Admin/SupportTicketController.php
+++ app/Http/Controllers/Api/V1/Admin/SupportTicketController.php
@@ -133,7 +133,7 @@ class SupportTicketController extends Controller
             ]);
 
             // Send email notification to support team
-            Mail::to(['support@snapshotalbums.net', 'snapshotalbums2023@gmail.com'])
+            Mail::to(['umarzahid028@gmail.com'])
                 ->send(new NewTicketMail($ticket));
 
             return response()->json([
