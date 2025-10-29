8ac5597af3c6ba3ff1ce0edaa6b584b95286d94c
Umar Zahid <umarzahid028@gmail.com>
2025-10-29 15:56:34 +0500

    cors update

--- config/cors.php
+++ config/cors.php
@@ -13,6 +13,7 @@ return [
         'http://127.0.0.1:8000',
         'http://localhost:5174',
         'http://127.0.0.1:5174',
+        'https://snapshotalbums-frontend.vercel.app',
         'https://snapshotalbums.vercel.app',
         'https://apis.snapshotalbums.net',
         'https://snapshotalbums.net'
