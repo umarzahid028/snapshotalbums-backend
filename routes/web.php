0e332724ff336fd1281c73d84f56a8e0e209e8e7
Umar Zahid <umarzahid028@gmail.com>
2025-11-05 00:17:34 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
    return response('Welcome to the snapshotalbums. This route is for testing purposes.');
 
 });
 
+Route::get('/test', function () {
+    Mail::to('umarzahid02*@gmail.com')->send(new EventReminderMail(Album::find(1)));
+ });
 
